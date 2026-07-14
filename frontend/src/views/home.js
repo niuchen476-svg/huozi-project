5a311caca18ef59f9a3dd18e24c268fecd17ecdf
niuchen <niuchen476@gamil.com>
2026-07-13 19:03:51 -0700

    refactor: simplify cover entry

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -9,12 +9,11 @@ export function renderHomeView(root) {
       <div class="view-home__content">
         <header class="map-header">
           <p class="map-header__eyebrow">数字图书馆馆员任务档案</p>
-          <h1>长征档案行</h1>
+          <h1>重走长征路</h1>
           <p class="map-header__subtitle">重建长征关键路线 · 用史料检验你的每一次判断</p>
         </header>
         <div class="home-actions">
           <button type="button" id="join-army" class="home-cta">加入红军，一起重温长征路</button>
-          <a class="home-link" href="#/landing">查看项目介绍</a>
         </div>
       </div>
     </div>
